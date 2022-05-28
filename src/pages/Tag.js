f25b7d0d77121685f17d864c42e779bf481ece64
Larcos665 <latombe8@misena.edu.co>
2022-05-27 21:52:04 -0500

    Taglist advance

---
+++ src/pages/Tag.js
@@ -0,0 +1,36 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import axios from 'axios';
+
+/** Componente dummy usando el API dummy de Tasky */
+class TagPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/tags').then((res) => {
+      const tags = res.data;
+      this.setState({ tags });
+    });
+  }
+
+  render() {
+    const { tags } = this.state;
+    return (
+      <Box sx={{ flexGrow: 1, p: 6 }}>
+        <h1>Tags</h1>
+        <ul>
+          {tags.map((tag) => (
+            <li>{tag.name}</li>
+          ))}
+        </ul>
+      </Box>
+    );
+  }
+}
+
+export default TagPage;
